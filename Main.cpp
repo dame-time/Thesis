c40e2010a8f692f3b11568897839355e6fbf472c
Davide Paollilo <davidepaollilo@MBP-di-Davide.station>
2023-02-11 20:46:37 +0100

    Updated dummy Main with test shader creation

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include <Window.hpp>
+#include <Shader.hpp>
 
 int main() {
     System::Window* window = new System::Window(800, 600, "MyApplication", false);
+
+    System::Shader *testShader = new System::Shader("./Shaders/test.vert", "./Shaders/test.frag");
+    testShader->use();
 
     while (!glfwWindowShouldClose(window->window)) {
         window->clear();
